afb6b9f630a5c9deeb2d1f61bd0c5030aa791abe
Andrea Maceratesi <andrea.maceratesi@gmail.com>
2018-11-29 19:47:05 +0100

    Add test task to pipeline

--- ProductsApp/Models/Product.cs
+++ ProductsApp/Models/Product.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
-        public decimal Price { get; set; }1
+        public decimal Price { get; set; }
     }
 }
